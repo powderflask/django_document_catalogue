3d41fe140ccbd457a1c0263e5894508032ee6205
powderflask <powderflask@gmail.com>
2020-05-25 15:45:25 -0700

    docs config

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'demo.settings.defaults'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'docs.settings'
 django.setup()
 
 # -- Project information -----------------------------------------------------
