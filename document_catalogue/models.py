b12a4a134639e5af030f507ec5f2fccbb796ea8e
powderflask <powderflask@gmail.com>
2020-10-26 12:32:10 -0700

    v.2.6

--- document_catalogue/models.py
+++ document_catalogue/models.py
@@ -136,7 +136,6 @@ class Document(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.sort_order:
-            qs = Document.objects.filter(category=self.category) if self.category else Document.objects
-            last = qs.order_by('sort_order').values('sort_order').last()
+            last = Document.objects.order_by('sort_order').values('sort_order').last()
             self.sort_order = last['sort_order'] + 1 if last else 1
         super().save(*args, **kwargs)
