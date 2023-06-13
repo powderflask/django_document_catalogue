15eb5edf17b4fd6bdd4e5f3e60c1132c981f4a17
Ben Blair <benchrisblair@users.noreply.github.com>
2023-06-13 06:26:21 -0700

    Removed setup.py usages

--- tasks/pypi.py
+++ tasks/pypi.py
@@ -2,57 +2,62 @@ import re
 from invoke import task
 from . import docs as docs_task
 
-def setup(c, command):
-    c.run('python setup.py {}'.format(command))
 
 @task
 def clean(c, docs=True):
     """ Clean setup / build directories """
-    setup(c, 'clean')
+    c.run('invoke clean')
     if docs:
         docs_task.clean(c)
+
 
 @task(clean)
 def build(c, docs=False):
     """ Clean and build a new distribution """
-    setup(c, "sdist")
+    c.run('python -m build')
     if docs:
         docs_task.build(c)
 
+
 def get_versions():
     """ Grab version numbers from various places they are explicitly defined and return dictionary """
-    from document_catalogue import __version__
+    from document_catalogue import __version__  # FIXME: ModuleNotFoundError importing from document_catalogue
 
     with open("docs/.readthedocs.yml", "rb") as f:
-        DOCS_VERSION = str(re.search('version: (.+)', f.read().decode()).group(1))
+        docs_version = str(re.search('version: (.+)', f.read().decode()).group(1))
 
     return {
-        'Package document-catalogue Version' : __version__,
-        'Docs version' : DOCS_VERSION,
+        'Package version': __version__,
+        'Docs version': docs_version,
     }
+
 
 @task
 def version(c):
     """ Print current project versions found in source file(s) """
-    for k,v in get_versions().items():
+    for k, v in get_versions().items():
         print('{label}: {version}'.format(label=k, version=v))
+
 
 @task
 def upload(c, repo='testpypi'):
-    """ Upload build to given PyPi repo"""
+    """ Upload build to given PyPI repo"""
     c.run('twine upload --repository {} dist/*'.format(repo))
 
-@task(help={'dist': "Name of distirbution file under dist/ directory to check."})
+
+@task(help={'dist': "Name of distribution file under dist/ directory to check."})
 def check(c, dist):
     """ Twine check the given distribution """
     c.run('twine check dist/{}'.format(dist))
 
+
 @task(help={'repo': "Specify:  pypi  for a production release."})
 def release(c, repo='testpypi'):
-    print('Building new release and upload to {}'.format(repo))
+    """ Build release and upload to PyPI """
+    print('Building new release and uploading to {}'.format(repo))
     print('Current version in source:')
     version(c)
-    if input('Continue? y/N ').lower()[0] != 'y':
+    if input('Continue? (y/n): ').lower()[0] != 'y':
         print('Release aborted')
         exit(0)
     build(c)
