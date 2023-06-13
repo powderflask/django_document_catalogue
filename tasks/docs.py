78731c3feec7378f483895369375ce9389ba0f71
Ben Blair <benchrisblair@users.noreply.github.com>
2023-06-13 06:25:30 -0700

    Formatting changes

--- tasks/docs.py
+++ tasks/docs.py
@@ -1,20 +1,23 @@
-from invoke import task, Collection
+from invoke import task
+
 
 def make(c, command):
     c.run('cd docs; make {}'.format(command))
+
 
 @task
 def clean(c):
     """ Clean docs build """
     make(c, 'clean')
 
+
 @task(clean)
 def build(c):
     """ Clean and build Sphinx docs """
     make(c, 'html')
 
+
 @task(build)
 def release(c):
-    """ Push docs to github, triggering webhook to build readthedocs """
+    """ Push docs to GitHub, triggering webhook to build Read The Docs """
     c.run('git push')
-
