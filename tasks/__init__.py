97f2afa55ccfc74bacca9fd20c7843b8d0c6b186
Ben Blair <benchrisblair@users.noreply.github.com>
2023-06-29 16:20:00 -0700

    Removed FIXMEs for Windows

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -7,7 +7,7 @@ from . import docs, pypi
 @task
 def clean(c):
     """ Clean up the project root directory """
-    if platform.system() == 'Windows':
+    if platform.system() == 'Windows':  # FIXME: Remove code for Windows
         c.run('PowerShell "Remove-Item -Verbose -Recurse -Force ./*.pyc, ./*.egg-info, ./*.pytest_cache, ./*.tox"')
     else:
         c.run('rm -vrf ./*.pyc ./*.egg-info ./*.pytest_cache ./*.tox')
